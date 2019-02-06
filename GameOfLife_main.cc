141a738acd7922c7779856a91ae79a6c6a6c4c11
FeiFei Hagen <fei-fei.hagen@zephaniah.gordon.edu>
2019-02-06 11:55:11 -0500

    added the print statements in this file, but still has some errors to it

--- GameOfLife_main.cc
+++ GameOfLife_main.cc
@@ -3,18 +3,77 @@
 // CPS222 Project 1
 
 #include <iostream>
+using std::cin;
 using std::cout;
 using std::endl;
 
 #include "GameOfLife.h"
-#include "GameOfLife.cc"
 
-int main() {
-	int numberOfOrganisms;
-	int row;
-	int col;
-	int gen;
-	
-	printBoard();
-	updateBoard();
-}
+
+int main()
+ {
+     int numberOfOrganisms;
+     int row;
+     int col;
+     int gen;
+  
+     //printBoard();
+     //updateBoard();
+
+     cout << "How many organisms initially? ";
+     cin >> numberOfOrganisms;
+
+     while (cin.get() != '\n') 
+     {   // intentionally empty loop body
+     }
+
+     cout << "Locations? ";
+
+     for (int x = 0; x < totalRows; x++)
+     {
+        for (int y = 0; y < totalCols; y++)
+        {
+         _board[x][y] == 0;
+        }
+
+      cout << endl;
+      return;
+     }
+
+      cin >> _board[x][y];
+
+      while (cin.get() != '\n')
+      {   // intentionally empty loop body
+      }
+
+      cout << "Generations? ";
+      cin >> gen;
+
+      while (cin.get() != '\n')
+      {   // intentionally empty loop body
+      }
+
+      cout << ESC << "[H" << "Generation " << gen << ":" << endl;
+      cout << ESC << "[H" << ESC << "[J" << "Initial:" << endl;
+
+      cout << "+--------------------------------------------------+" << endl;
+
+      for (int i = 0; i < activeRows; i++)
+      {
+        cout << "|";
+          for (int j = 0; j < activeCols; j++)
+          {
+            cout << " ";
+          }
+        cout << "|";
+        cout << endl;
+       }
+
+      cout << "+--------------------------------------------------+" << endl;
+
+      cout << ESC << "[23;1H" << ESC << "[K" << "Press RETURN to continue";
+
+      while (cin.get() != '\n')
+      {   // intentionally empty loop body
+      }
+  }
