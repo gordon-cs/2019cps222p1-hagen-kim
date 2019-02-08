cd9a33c2f380a9c9a132f8ba66048425c561cd9b
FeiFei Hagen <fei-fei.hagen@zephaniah.gordon.edu>
2019-02-08 05:53:03 -0500

    saving a few changes - not sure if we need to define the variables in this file

--- GameOfLife.cc
+++ GameOfLife.cc
@@ -3,39 +3,47 @@
 #include <vector>
 #include "GameOfLife.h"
 
-
 using std::cout;
 using std::endl;
 
 using namespace std;
 
+
 // Initialize the board
-Board::Board()
+/*Board::Board(const int tRows, const int tCols):
 {
-    for (int r = 0; r < totalRows; r++)
+     totalRows(tRows),
+     totalCols(tCols)
+}
+*/
+
+void Board(Organism _board[totalRows][totalCols])
+{
+    for (int r = 0; r < totalRows-2; r++)
     {
-        for (int c = 0; c < totalCols; c++)
+        for (int c = 0; c < totalCols-2; c++)
         {
-             _board[r][c] = 0;
+             cout << _board[r][c];
+        }
+     }  cout << endl;
+
+
+
+
+/*void displayBoard()
+{
+    for (int i = 0; i < totalRows; totalRows++)
+    {
+        for (int j = 0; j < totalCols; totalCols++)
+        {
+             _board[i][j] = 0;
         }
     }
-};
-
-void displayBoard(vector< vector<char> > &_board)
-{
-    for (int i = 0; i < x; i++)
-    {
-        cin >> row;
-        cin >> col;
-
-        _board[row][col] = char;
-     }
-}
+};*/
 
 
 // counts the living organisms
-
-GameOfLife :: state()
+void Board(Organism _board[totalRows][totalCols])
  {
     for (int r = 0; r < activeRows; r++ )
     {
@@ -75,50 +83,41 @@ GameOfLife :: state()
          {
             count = count + 1;
          }
-       }
-     }
 
 
-
-// counts the empty organisms
-
-
-  for (int r = 0; r < activeRows; r++ )
-  {
-    for (int c = 0; c < activeCols; c++)
-    {
-      int count = 0;
-      if (_board[r-1][c-1] == NONE)
-      {
-        count = count + 1;
-      }
-      else if (_board[r][c-1] == NONE)
-      {
-        count = count + 1;
-      }
-      else if (_board[r+1][c-1] == NONE)
-      {
-        count = count + 1;
-      }
-      else if (_board[r-1][c] == NONE)
-      {
-        count = count + 1;
-      }
-      else if (_board[r+1][c] == NONE)
-      {
-        count = count + 1;
-      }
-      else if (_board[r-1][c+1] == NONE)
-      {
-        count = count + 1;
-      }
-      else if (_board[r][c+1] == NONE)
-      {
-        count = count + 1;
-      }
-      else if (_board[r+1][c+1] == NONE)
-      {
-        count = count + 1;
+         updateBoard[r][c] = NONE;
+         // counts the empty organisms
+         if (_board[r-1][c-1] == NONE)
+         {
+           count = count + 1;
+         }
+         else if (_board[r][c-1] == NONE)
+         {
+           count = count + 1;
+         }
+         else if (_board[r+1][c-1] == NONE)
+         {
+           count = count + 1;
+         }
+         else if (_board[r-1][c] == NONE)
+         {
+           count = count + 1;
+         }
+         else if (_board[r+1][c] == NONE)
+         {
+           count = count + 1;
+         }
+         else if (_board[r-1][c+1] == NONE)
+         {
+          count = count + 1;
+         }
+         else if (_board[r][c+1] == NONE)
+         {
+          count = count + 1;
+         }
+         else if (_board[r+1][c+1] == NONE)
+         {
+          count = count + 1;
       }
     }
   }
