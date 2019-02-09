71e7acefec12fc02161d30efc356ad2875a66cd6
Jenny Kim <jenny.kim@gordon.edu>
2019-02-08 23:31:28 -0500

    Updating constants and the board

--- GameOfLife.h
+++ GameOfLife.h
@@ -9,23 +9,27 @@
 #include <iostream>
 using std::istream;
 
-enum Organism { NONE, LIVING };
-Organism _board[totalRows][totalCols];
+enum class Organism { NONE, LIVING };
 
 // stores and manipulates the board and state of life game
 class Board {
-
 private:
-	// definition of what each of the constants represent
+	// definition of constants that will not change
 	static const int activeRows = 18;
 	static const int activeCols = 50;
-	static const int totalRows = activeRows + 2;
-	static const int totalCols = activeCols + 2;
 	static const char ESC = 27;
+	
+	// definition of constants that might change
+	static int totalRows = activeRows + 2;
+	static int totalCols = activeCols + 2;
+	static int r;
+	static int c;
+	static int numberOfOrganisms;
 
 	// how each of the constants is displayed on the board 
 	static const char LIVING = "*";
 	static const char NONE = " ";
+	Organism _board[totalRows][totalCols];
   
 // Constructor  (same name as class, no return type, no need to specify constructor
 public:
@@ -37,10 +41,8 @@ public:
 
  // Updates the Game Of Life board
  public:
-  void updateBoard();
+        void updateBoard();
 
- private:
-    Organism _board[totalRows][totalCols];
 };
 
 
